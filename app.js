a83d78e2228feb799dff262c66eceacc3cb8306c
Tim <s87462830@gmail.com>
2021-11-07 19:32:23 +0800

    feat: add relation between 'users' & 'todos'

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const PORT = 3000
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 
 app.get('/', (req, res) => {
