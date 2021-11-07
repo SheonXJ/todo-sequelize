d3e30ad7453ebe668377d64e083177eb150515c6
Tim <s87462830@gmail.com>
2021-11-07 19:18:43 +0800

    feat: sequelize init

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
-const bodyParser = require('body-parser')
+// const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
 const bcrypt = require('bcryptjs')
 
